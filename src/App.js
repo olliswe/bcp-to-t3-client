dc2146c6a95110e0da9b3d0813fcd5e1f61f1113
parrr.wei.geng <parrr.wei.geng@gmail.com>
2024-11-12 21:57:13 +0100

    feat: handle all promises properly and adjust UI

--- src/App.js
+++ src/App.js
@@ -5,41 +5,52 @@ import CsvDownload from "react-json-to-csv";
 
 const API_URL = "https://bcp-to-t3-api.onrender.com";
 
-function App() {
+const App = () => {
   const [eventIds, setEventIds] = useState("");
-  const [eventData, setEventData] = useState(null);
+  const [eventData, setEventData] = useState({ data: null, filename: "" });
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState("");
 
   const handleSubmit = async () => {
     setLoading(true);
-    setEventData(null);
+    setEventData({ data: null, filename: "" });
     setError("");
 
     const eventIdArr = eventIds.replaceAll(' ', '').split(',')
-    const combinedEventData = []
-    eventIdArr.forEach(async(id, index) => {
-      const isLast = index === eventIdArr.length - 1
-      try {
-        const res = await axios.get(`${API_URL}/v2/bcp-event?eventId=${id}`);
-        if (res?.data?.success) {
-          combinedEventData.push(...res.data.data)
+    let combinedEventData = []
+
+    try {
+      const eventPromises = eventIdArr.map(async (id, index) => {
+        const isLast = index === eventIdArr.length - 1
+
+        const { data } = await axios.get(`${API_URL}/v2/bcp-event?eventId=${id}`)
+        if ( data?.success ) {
           if (isLast) {
             setEventIds("")
-            setLoading(false);
           }
-          return
+          return {
+            data: data.data,
+            name: data.data?.[0].tournament_name
+          }
+        } else {
+          throw new Error("Unable to get event data")
         }
-        setError("Unable to get event data");
-        setLoading(false);
-      } catch (error) {
-        setError(error.toString());
-        setLoading(false);
-      }
-    })
+      })
 
-    setEventData(combinedEventData)
-  };
+      combinedEventData = await Promise.all(eventPromises)
+
+      const eventData = {
+        data: combinedEventData.map(event => event.data).flat(),
+        filename: `${combinedEventData.map(event => event.name).join(", ") || "unknown"}.csv`
+      }
+      setEventData(eventData)
+    } catch (error) {
+      setError(error.toString())
+    } finally {
+      setLoading(false)
+    }
+  }
+
   return (
     <div style={{ display: "flex", flexDirection: "column", margin: "20px" }}>
       <h1>BCP TO GERMAN RANKINGS EXPORT TOOL</h1>
@@ -48,7 +59,7 @@ function App() {
           value={eventIds}
           onChange={(event) => setEventIds(event.target.value)}
           placeholder={"Enter BCP event ID (e.g. zYnDUrjild)"}
-          style={{ width: "800px" }}
+          style={{ width: "800px", marginRight: "5px" }}
         />
         <button onClick={handleSubmit} disabled={loading}>
           {loading ? "Loading..." : "Submit"}
@@ -59,12 +70,15 @@ function App() {
           <span style={{ color: "red" }}>{error}</span>
         </div>
       )}
-      {eventData && (
-        <div style={{ marginTop: "20px" }}>
-          <span>Success! </span>
+      {eventData.data && !loading && (
+        <div style={{ display: "flex", flexDirection: "row", marginTop: "20px" }}>
+          <div style={{ width: "808px", marginRight: "5px" }}>
+            <span>Success to download: </span>
+            <span style={{ fontWeight: "bold", color: "green" }}>{eventData.filename}</span>
+          </div>
           <CsvDownload
-            data={eventData}
-            filename={`${eventData[0]?.tournament_name || "unknown"}.csv`}
+            data={eventData.data}
+            filename={eventData.filename}
           />
         </div>
       )}
